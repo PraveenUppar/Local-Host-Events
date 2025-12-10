d3628b11669a196916fab4dd6a8a7d8ccf970517
Praveen Uppar <praveenuppar718@gmail.com>
2025-12-10 13:16:55 +0530

    fix7

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  staticPageGenerationTimeout: 100,
-  experimental: {
-    serverActions: {
-      allowedOrigins: ["pavicodes.in", "localhost:3000"],
-    },
-  },
 };
 
 export default nextConfig;
